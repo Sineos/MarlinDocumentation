b689753f8860c1624053495eafdffcd819ef3983
Sineos <monk@gmx.net>
2017-11-21 07:28:40 +0100

    Rotate
    - Add option to rotate the test pattern
    - Add option to modify test line spacing
    - Add option for anchor frame

--- _tools/lin_advance/k-factor.js
+++ _tools/lin_advance/k-factor.js
@@ -16,39 +16,72 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 function gengcode(form1) {
-    
+
     var FILAMENT_DIAMETER = parseFloat(document.forms['form1']['FIL_DIA'].value);
     var NOZZLE_DIAMETER = parseFloat(document.forms['form1']['NOZ_DIA'].value);
     var NOZZLE_TEMP = parseInt(document.forms['form1']['NOZZLE_TEMP'].value);
-    var NOZZLE_LINE_RATIO = parseFloat(document.forms['form1']['NOZ_LIN_R'].value);  
+    var NOZZLE_LINE_RATIO = parseFloat(document.forms['form1']['NOZ_LIN_R'].value);
     var BED_TEMP = parseInt(document.forms['form1']['BED_TEMP'].value);
     var SPEED_SLOW = parseInt(document.forms['form1']['SLOW_SPEED'].value);
     var SPEED_FAST = parseInt(document.forms['form1']['FAST_SPEED'].value);
-    var SPEED_MOVE = parseInt(document.forms['form1']['MOVE_SPEED'].value); 
+    var SPEED_MOVE = parseInt(document.forms['form1']['MOVE_SPEED'].value);
     var RETRACT_DIST = parseFloat(document.forms['form1']['RETRACTION'].value);
     var BED_X = parseInt(document.forms['form1']['BEDSIZE_X'].value);
     var BED_Y = parseInt(document.forms['form1']['BEDSIZE_Y'].value);
-    var HEIGHT_LAYER = parseFloat(document.forms['form1']['LAYER_HEIGHT'].value);   
+    var HEIGHT_LAYER = parseFloat(document.forms['form1']['LAYER_HEIGHT'].value);
     var EXT_MULT = parseFloat(document.forms['form1']['EXTRUSION_MULT'].value);
     var START_K = parseInt(document.forms['form1']['K_START'].value);
     var END_K = parseInt(document.forms['form1']['K_END'].value);
     var STEP_K = parseFloat(document.forms['form1']['K_STEP'].value);
+    var SELECT_DIR = document.getElementById('DIR_PRINT');
+    var PRINT_DIR = SELECT_DIR.options[SELECT_DIR.selectedIndex].value;
+	var LINE_SPACING = parseFloat(document.forms['form1']['SPACE_LINE'].value);
+
+
+    var RANGE_K =  END_K - START_K;
+    var LINE_WIDTH = NOZZLE_DIAMETER * NOZZLE_LINE_RATIO;
+    var PRINT_SIZE = RANGE_K / STEP_K * 5 + 25;
+    var CENTER_X = BED_X / 2;
+    var CENTER_Y = BED_Y / 2;
+    var PRIME_START_X = CENTER_X - 60;
+    var PRIME_START_Y = CENTER_Y - PRINT_SIZE / 2;
+    var PRIME_END_X = CENTER_X - 60;
+    var PRIME_END_Y = CENTER_Y + PRINT_SIZE / 2;
+    var REF1_START_X = CENTER_X - 20;
+    var REF2_START_X = CENTER_X + 20;
+    var REF_START_Y = PRINT_SIZE / 2 + CENTER_Y - 20;
+    var REF_END_Y = PRINT_SIZE / 2 + CENTER_Y;
+    var PAT_START_X = CENTER_X - 40;
+    var PAT_START_Y = CENTER_Y - PRINT_SIZE / 2;
+    var FRAME_START_X = CENTER_X - 40;
+    var FRAME_START_Y = CENTER_Y - PRINT_SIZE / 2;
+    var FRAME_END_X = CENTER_X - 40;
+    var FRAME_END_Y = CENTER_Y + PRINT_SIZE / 2;
 
     // Check if K-Factor Stepping is a multiple of the K-Factor Range
-    var RANGE_K =  END_K - START_K;
+
     if (RANGE_K % STEP_K != 0) {
       alert("Your K-Factor range cannot be cleanly divided. Check Start / End / Steps for the K-Factor");
       document.forms['form1']['textarea'].value = '';
       return;
-    }   
-    // Check if the Test Pattern size does not exceed the Y Bed Size
-    var PRINT_SIZE_Y = RANGE_K / STEP_K * 5 + 25;
-    if (PRINT_SIZE_Y > BED_Y - 20) {
-      alert("Your K-Factor settings exceed your Y bed size. Check Start / End / Steps for the K-Factor");
-      document.forms['form1']['textarea'].value = '';
-      return;
     }
-    var START_Y = (BED_Y - PRINT_SIZE_Y) / 2;   
+    // Check if the Test Pattern size does not exceed the Bed Size
+
+    if (DIR_PAT === "X") {
+      if (PRINT_SIZE > BED_Y - 20) {
+        alert("Your K-Factor settings exceed your Y bed size. Check Start / End / Steps for the K-Factor");
+        document.forms['form1']['textarea'].value = '';
+        return;
+      }
+      var START_Y = (BED_Y - PRINT_SIZE) / 2;
+    } else {
+      if (PRINT_SIZE > BED_X - 20) {
+        alert("Your K-Factor settings exceed your X bed size. Check Start / End / Steps for the K-Factor");
+        document.forms['form1']['textarea'].value = '';
+        return;
+      }
+      var START_X = (BED_X - PRINT_SIZE) / 2;
+    }
 
     // Convert speeds from mm/s to mm/min if needed
     if (document.getElementById('MM_S').checked) {
@@ -57,11 +90,12 @@ function gengcode(form1) {
       SPEED_MOVE = SPEED_MOVE * 60;
     }
 
-    //Set the extrusion parameters
-    var EXTRUSION_RATIO = NOZZLE_DIAMETER * NOZZLE_LINE_RATIO * HEIGHT_LAYER / (Math.pow(FILAMENT_DIAMETER / 2,2) * Math.PI);
+     //Set the extrusion parameters
+    var EXTRUSION_RATIO = LINE_WIDTH * HEIGHT_LAYER / (Math.pow(FILAMENT_DIAMETER / 2,2) * Math.PI);
+    var EXT_PRIME = roundNumber(EXTRUSION_RATIO * EXT_MULT * (PRIME_END_Y - PRIME_START_Y), 5);
     var EXT_20 = roundNumber(EXTRUSION_RATIO * EXT_MULT * 20, 5);
     var EXT_40 = roundNumber(EXTRUSION_RATIO * EXT_MULT * 40, 5);
-    
+
     document.forms['form1']['textarea'].value = '';
     document.forms['form1']['textarea'].value = '; K-Factor Test\n' +
                                                 ';\n' +
@@ -85,56 +119,57 @@ function gengcode(form1) {
                                                 '; Ending value K-Factor = ' + END_K + '\n' +
                                                 '; K-Factor Stepping = ' + STEP_K + '\n' +
                                                 ';\n' +
-                                                'G28 ; home all axes\n' +                                                   
+                                                'G28 ; home all axes\n' +
                                                 'M190 S' + BED_TEMP + ' ; set and wait for bed temp\n' +
                                                 'M104 S' + NOZZLE_TEMP + ' ; set nozzle temp and continue\n';
 
-    if (document.getElementById('USE_UBL').checked)
+    if (document.getElementById('USE_UBL').checked) {
       document.forms['form1']['textarea'].value += 'G29 ; execute bed automatic levelling compensation\n';
-    
+    }
+
     document.forms['form1']['textarea'].value += 'M109 S' + NOZZLE_TEMP + ' ; block waiting for nozzle temp\n' +
                                                  'G21 ; set units to millimetres\n' +
                                                  'M204 S500 ; lower acceleration to 500mm/s2 during the test\n' +
-                                                 'M83 ; use relative distances for extrusion\n' + 
+                                                 'M83 ; use relative distances for extrusion\n' +
                                                  'G90 ; use absolute coordinates\n' +
                                                  ';\n' +
                                                  '; go to layer height and prime nozzle on a line to the left\n' +
                                                  ';\n' +
-                                                 'G1 X' + ((BED_X - 120) / 2) + ' Y' + START_Y + ' F' + SPEED_MOVE + '\n' +
+                                                 'G1 X' + rotateX(PRIME_START_X, CENTER_X, PRIME_START_Y, CENTER_Y, PRINT_DIR) + ' Y' + rotateY(PRIME_START_X, CENTER_X, PRIME_START_Y, CENTER_Y, PRINT_DIR) + ' F' + SPEED_MOVE + '\n' +
                                                  'G1 Z' + HEIGHT_LAYER + ' F' + SPEED_SLOW + '\n' +
-                                                 'G1 X' + ((BED_X - 120) / 2) + ' Y' + (START_Y + 100) + ' E10' + ' F' + SPEED_SLOW + ' ; extrude some to start clean\n' +
+                                                 'G1 X' + rotateX(PRIME_END_X, CENTER_X, PRIME_END_Y, CENTER_Y, PRINT_DIR) + ' Y' + rotateY(PRIME_END_X, CENTER_X, PRIME_END_Y, CENTER_Y, PRINT_DIR) + ' E' + EXT_PRIME + ' F' + SPEED_SLOW + ' ; extrude some to start clean\n' +
                                                  'G1 E-' + RETRACT_DIST + '\n' +
                                                  ';\n' +
                                                  '; start the test (all values are relative coordinates)\n' +
-                                                 ';\n' +
-                                                 'G1 X' + ((BED_X - 80) / 2) + ' Y' + START_Y + ' F' + SPEED_MOVE + ' ; move to pattern start\n' +
-                                                 'G91 ; use relative coordinates\n';
-    
-    
-    for (i = START_K; i <= END_K; i = i + STEP_K)
+                                                 ';\n';
+                                                 //'G1 X' + rotateX(PAT_START_X, CENTER_X, PRINT_DIR) + ' Y' + rotateY(PAT_START_Y, CENTER_Y, PRINT_DIR) + ' F' + SPEED_MOVE + ' ; move to pattern start\n' +
+                                                // 'G91 ; use relative coordinates\n';
+    var j = 0;
+    for (i = START_K; i <= END_K; i = i + STEP_K) {
         document.forms['form1']['textarea'].value += 'M900 K' + i + ' ; set K-factor\n' +
+                                                     'G1 X' + rotateX(PAT_START_X, CENTER_X, PAT_START_Y + j, CENTER_Y, PRINT_DIR) + ' Y' + rotateY(PAT_START_X, CENTER_X, PAT_START_Y + j, CENTER_Y, PRINT_DIR) + ' F' + SPEED_MOVE + ' ; move to pattern start\n' +
                                                      'G1 E' + RETRACT_DIST + '\n' +
-                                                     'G1 X20 Y0 E' + EXT_20 + ' F' + SPEED_SLOW + '\n' +
-                                                     'G1 X40 Y0 E' + EXT_40 + ' F' + SPEED_FAST + '\n' +
-                                                     'G1 X20 Y0 E' + EXT_20 + ' F' + SPEED_SLOW + '\n' +
-                                                     'G1 E-' + RETRACT_DIST + '\n' +
-                                                     'G1 X-80 Y5 F' + SPEED_MOVE + '\n';
-    
+                                                     'G1 X' + rotateX(PAT_START_X + 20, CENTER_X, PAT_START_Y + j, CENTER_Y, PRINT_DIR) + ' Y' + rotateY(PAT_START_X + 20, CENTER_X, PAT_START_Y + j, CENTER_Y, PRINT_DIR) + ' E' + EXT_20 + ' F' + SPEED_SLOW + '\n' +
+                                                     'G1 X' + rotateX(PAT_START_X + 60, CENTER_X, PAT_START_Y + j, CENTER_Y, PRINT_DIR) + ' Y' + rotateY(PAT_START_X + 60, CENTER_X, PAT_START_Y + j, CENTER_Y, PRINT_DIR) + ' E' + EXT_40 + ' F' + SPEED_FAST + '\n' +
+                                                     'G1 X' + rotateX(PAT_START_X + 80, CENTER_X, PAT_START_Y + j, CENTER_Y, PRINT_DIR) + ' Y' + rotateY(PAT_START_X + 80, CENTER_X, PAT_START_Y + j, CENTER_Y, PRINT_DIR) + ' E' + EXT_20 + ' F' + SPEED_SLOW + '\n' +
+                                                     'G1 E-' + RETRACT_DIST + '\n';
+        j = j + 5;
+    }
+
     document.forms['form1']['textarea'].value += ';\n' +
                                                  '; mark the test area for reference\n' +
                                                  ';\n' +
-                                                 'G1 X20 Y0 F' + SPEED_MOVE + '\n' +
+                                                 'G1 X' + rotateX(REF1_START_X, CENTER_X, REF_START_Y, CENTER_Y, PRINT_DIR) + ' Y' + rotateY(REF1_START_X, CENTER_X, REF_START_Y, CENTER_Y, PRINT_DIR) + ' F' + SPEED_MOVE + '\n' +
                                                  'G1 E' + RETRACT_DIST + '\n' +
-                                                 'G1 X0 Y20 E' + EXT_20 + ' F' + SPEED_SLOW + '\n' +
+                                                 'G1 X' + rotateX(REF1_START_X, CENTER_X, REF_END_Y, CENTER_Y, PRINT_DIR) + ' Y' + rotateY(REF1_START_X, CENTER_X, REF_END_Y, CENTER_Y, PRINT_DIR) + ' E' + EXT_20 + ' F' + SPEED_SLOW + '\n' +
                                                  'G1 E-' + RETRACT_DIST + '\n' +
-                                                 'G1 X40 Y-20 F' + SPEED_MOVE + '\n' +
+                                                 'G1 X' + rotateX(REF2_START_X, CENTER_X, REF_START_Y, CENTER_Y, PRINT_DIR) + ' Y' + rotateY(REF2_START_X, CENTER_X, REF_START_Y, CENTER_Y, PRINT_DIR) + ' F' + SPEED_MOVE + '\n' +
                                                  'G1 E' + RETRACT_DIST + '\n' +
-                                                 'G1 X0 Y20 E' + EXT_20 + ' F' + SPEED_SLOW + '\n' +
+                                                 'G1 X' + rotateX(REF2_START_X, CENTER_X, REF_END_Y, CENTER_Y, PRINT_DIR) + ' Y' + rotateY(REF2_START_X, CENTER_X, REF_END_Y, CENTER_Y, PRINT_DIR) + ' E' + EXT_20 + ' F' + SPEED_SLOW + '\n' +
                                                  'G1 E-' + RETRACT_DIST + '\n' +
                                                  ';\n' +
                                                  '; finish\n' +
                                                  ';\n' +
-                                                 'G4 ; wait\n' +
                                                  'M104 S0 ; turn off hotend\n' +
                                                  'M140 S0 ; turn off bed\n' +
                                                  'G90 ; use absolute coordinates\n' +
@@ -175,12 +210,36 @@ function destroyClickedElement(event) {
 
 // https://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-only-if-necessary
 function roundNumber(num, scale) {
-  if (!("" + num).includes("e")) {
-    return +(Math.round(num + "e+" + scale)  + "e-" + scale);
-  } else {
-    var arr = ("" + num).split("e");
-    var sig = ""
-    if (+arr[1] + scale > 0) sig = "+";
-    return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
-  }
+    if (!("" + num).includes("e")) {
+      return +(Math.round(num + "e+" + scale)  + "e-" + scale);
+    } else {
+      var arr = ("" + num).split("e");
+      var sig = ""
+      if (+arr[1] + scale > 0) sig = "+";
+      return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
+    }
+}
+
+function rotateX(x, xm, y, ym, a) {
+    a = a * Math.PI / 180; // Convert to radians
+    var cos = Math.cos(a);
+    var sin = Math.sin(a);
+
+    // Subtract midpoints, so that midpoint is translated to origin
+    // and add it in the end again
+    //var xr = (x - xm) * cos - (y - ym) * sin + xm; //CCW
+    var xr = (cos * (x - xm)) + (sin * (y - ym)) + xm; //CW
+    return xr;
+}
+
+function rotateY(x, xm, y, ym, a) {
+    a = a * Math.PI / 180; // Convert to radians
+    var cos = Math.cos(a);
+    var sin = Math.sin(a);
+
+    // Subtract midpoints, so that midpoint is translated to origin
+    // and add it in the end again
+    //var yr = (x - xm) * sin + (y - ym) * cos + ym; //CCW
+    var yr = (cos * (y - ym)) - (sin * (x - xm)) + ym; //CW
+    return yr;
 }
